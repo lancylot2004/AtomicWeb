a3a9c0964ed7b37473a3d309d6bba784eaaf8fcb
trap101 <cheng050929@gmail.com>
2023-04-29 23:23:23 +0100

    janky minimap

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import style from "../styles/Questions.module.css";
 import  ReactMarkdown  from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import html2canvas from "html2canvas";
 // import remarkGfm from 'remark-gfm'
 const inter = Inter({ subsets: ["latin"] });
@@ -12,26 +12,37 @@ export async function getServerSideProps(){
   const data = await res.text()
   return {props:{data}}
 }
-
+let didInit = false
 export default function Home({data}:{data:string}) {
-  const questionBoard = useRef(null)
+  const questionBoard = useRef<HTMLDivElement>(null!)
+  const miniMapImage = useRef<HTMLImageElement>(null)
   useEffect(()=>{
-    // html2canvas(questionBoard.current,)
-    
-  })
+    if(!didInit){
+      html2canvas(questionBoard.current,{scale:3}).then(function(canvas){
+        let dataURL = canvas.toDataURL()
+        // let img = document.createElement("img")
+        // img.setAttribute("src",dataurl)
+        // img.className=style.minimap
+        // document.body.appendChild(img)
+        miniMapImage.current?.setAttribute("src",dataURL)
+        didInit = true
+        }
+      )
+    }
+  },[])
   return (
     <>
       <Head>
         <title>Atomic Challenge 2023</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-
-      </div>
       <main className={style.container}>
+        <div className={style.minimapContainer}>
+          <img ref={miniMapImage} className={style.minimapSource}/>
+
+        </div>
         <div className={style.sidebar}>
           <header>Atomic Challenge 2023</header>
-          
           <ul className={style.questionList}>
             <li>abc</li>
             <li>def</li>

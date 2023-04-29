1b3aafda69eb863fd14742ef1de11ad7d1b6ff2e
trap101 <cheng050929@gmail.com>
2023-04-29 21:53:09 +0100

    minimap feature and test data

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,22 +1,37 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import style from "../styles/Questions.module.css";
 import  ReactMarkdown  from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { useEffect, useRef } from "react";
+import html2canvas from "html2canvas";
 // import remarkGfm from 'remark-gfm'
 const inter = Inter({ subsets: ["latin"] });
-const markdown = "# https://github.com/facebook/react/blob/main/README.md**"
-export default function Home() {
+export async function getServerSideProps(){
+  const res = await fetch("https://raw.githubusercontent.com/facebook/react/main/README.md")
+  const data = await res.text()
+  return {props:{data}}
+}
+
+export default function Home({data}:{data:string}) {
+  const questionBoard = useRef(null)
+  useEffect(()=>{
+    // html2canvas(questionBoard.current,)
+    
+  })
   return (
     <>
       <Head>
         <title>Atomic Challenge 2023</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+
+      </div>
       <main className={style.container}>
         <div className={style.sidebar}>
           <header>Atomic Challenge 2023</header>
-
+          
           <ul className={style.questionList}>
             <li>abc</li>
             <li>def</li>
@@ -25,8 +40,8 @@ export default function Home() {
             <li>lmn</li>
           </ul>
         </div>
-        <div className={style.questionBoard}>
-            <ReactMarkdown children={markdown}></ReactMarkdown>
+        <div className={style.questionBoard} ref={questionBoard}>
+            <ReactMarkdown children={data} remarkPlugins={[remarkGfm]}></ReactMarkdown>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae explicabo recusandae provident soluta culpa impedit quaerat ea aspernatur, nihil, quis similique? Quam repudiandae ad nihil dignissimos quia veniam, rem iusto! Maiores optio dolores dicta aliquid reiciendis iure repellat fuga ipsam quos dignissimos vitae odio provident aliquam ab consequuntur, voluptates inventore?
         </div>
 

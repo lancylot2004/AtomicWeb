a57c8ff4e402558ea0f9e2ed87d3c628693f31a3
Lancelot Liu <lancylot2004@proton.me>
2023-04-30 13:06:44 +0100

    Attempted add more rendering to `Markdown`.

--- public/markdown/S1-PCS.md
+++ public/markdown/S1-PCS.md
@@ -2,3 +2,7 @@
 *Section 1: MtDscp Questions, Physics & Computer Science*
 
 ## 1.1: Fool the Bot **[4]**
+
+```
+let dumbPerson = Person("Lancelot")
+```
--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,23 +1,47 @@
-import ReactMarkdown from 'react-markdown';
-import { useEffect, useState } from 'react';
+import ReactMarkdown from "react-markdown";
+import { useEffect, useState } from "react";
+import gfm from "remark-gfm";
+import math from "remark-math";
+import katex from "rehype-katex";
+import mathjax from "rehype-mathjax";
 
 interface Props {
   filePath: string;
 }
 
-export default function Markdown({ filePath }: Props) {
+const MyComponent = (): JSX.Element => {
+  return <div>This is my custom component!</div>;
+};
+
+export default function Markdown({ filePath }: Props): JSX.Element {
   const [markdown, setMarkdown] = useState("");
 
   useEffect(() => {
     fetch(filePath)
-      .then(response => response.text())
-      .then(data => setMarkdown(data))
-      .catch(error => console.error(error));
+      .then((response: Response) => response.text())
+      .then((data: string) => setMarkdown(data))
+      .catch((error: Error) => console.error(error));
   }, [filePath]);
+
+  const renderers: ReactMarkdownProps["renderers"] = {
+    image: ({ src, alt }) => <img src={src} alt={alt} />,
+    video: ({ src, controls }) => (
+      <video src={src} controls={controls} style={{ maxWidth: "100%" }} />
+    ),
+    code: ({ language, value }) => (
+      <p>a</p>
+    ),
+    myComponent: MyComponent,
+  };
 
   return (
     <div>
-      <ReactMarkdown>{markdown}</ReactMarkdown>
+      <ReactMarkdown
+        plugins={[gfm, math]}
+        renderers={renderers}
+        children={markdown}
+        rehypePlugins={[mathjax, katex]}
+      />
     </div>
   );
 }

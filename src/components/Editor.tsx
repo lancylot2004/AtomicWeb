3cbfe96b6c36f8e9261d57f56922407ee4cdf69c
Lancelot Liu <lancylot2004@proton.me>
2023-04-30 15:00:15 +0100

    Randomised typed Regex.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,31 +3,51 @@ import config from "@/config.json";
 import styles from "@/styles/editor.module.css";
 
 interface Props {
-  fileName: string
+  fileName: string;
 }
 
 export default function Editor({ fileName }: Props) {
-  const [file, setFile] = useState(config.defaultFileName);
+  const [regex, setRegex] = useState(config.regexes[0]);
+  const [typedRegex, setTypedRegex] = useState("");
+  const [showTyping, setShowTyping] = useState(false);
 
   useEffect(() => {
-    setFile(fileName)
-  }, [fileName])
+    const intervalId = setInterval(() => {
+      let newRegex = regex;
+      while (newRegex === regex) {
+        const index = Math.floor(Math.random() * config.regexes.length);
+        newRegex = config.regexes[index];
+      }
+      setShowTyping(true);
+      setTypedRegex("");
+      for (let i = 0; i < newRegex.length; i++) {
+        setTimeout(() => setTypedRegex((prev) => prev + newRegex[i]), i * 50);
+      }
+      setTimeout(() => {
+        setRegex(newRegex);
+        setShowTyping(false);
+      }, newRegex.length * 50);
+    }, Math.floor(Math.random() * 20000) + 5000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className={styles.editor}>
       <div className={styles.header}>
         <span className={styles.filename}>{fileName}</span>
         <span className={styles.search}>
-          <span className={styles.query}>(?&lt;=\s)[A-Z]+(?=,)</span>
+          <span className={styles.query}>
+            {showTyping ? typedRegex : regex}
+          </span>
           <span className={styles.options}>
             <span>g</span>
           </span>
         </span>
       </div>
       <div className={styles.footer}>
-        <span className={styles.branch}>feature/deploy*</span>
-        <span className={styles.charset}>UTF-8 LF</span>
-        <span className={styles.mode}>TypeScript</span>
+        <span className={styles.branch}>{config.branch}</span>
+        <span className={styles.charset}>{config.charset}</span>
+        <span className={styles.mode}>{config.mode}</span>
       </div>
     </div>
   );
